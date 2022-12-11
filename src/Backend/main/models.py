72ef3afee3395b94a530030c588257f10c8cd897
AlamutuIbrahim <abbeyabiodun06@gmail.com>
2022-12-11 21:40:39 +0100

    Creted models

--- src/Backend/main/models.py
+++ src/Backend/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class File(models.Model):
+    filename = models.Filefield(upload_to='uploads/')
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    @property
+    def file_size(self):
+        return self.filename.size
+
+    @property
+    def file_type(self):
+        return self.filename.content_type
+
+    def __str__(self):
+        return f'{self.filename} - {self.filesize}'
