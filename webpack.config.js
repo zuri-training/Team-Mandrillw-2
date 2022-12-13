d4f978ed57c3edf3c10ae742edd53b71a97a6fdb
Unmarked-Kyuubi <marvelwise53@gmail.com>
2022-12-13 19:07:44 +0100

    FInally some progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require('path');
+const ExcelJS = require('exceljs');
+
+// const dependencies = [
+//    // other dependencies
+//    'exceljs'
+//  ];
+
 
 module.exports = {
   entry: './src/js/test.js',
+  dependencies: ["ExcelJS"],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
