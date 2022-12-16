cf0d4fb55a2d02f11ee5b1805e5bb989f2e22819
Unmarked-Kyuubi <marvelwise53@gmail.com>
2022-12-16 04:43:25 +0100

    Stufff

---
+++ src/js/edit-modal.js
@@ -0,0 +1,16 @@
+
+// create a function to show the modal
+function showModal() {
+  // get the modal element
+  var modal = document.getElementById('myModal');
+
+  // show the modal
+  modal.style.display = "block";
+}
+
+
+// add an event listener to the onload event of the page
+window.onload = function() {
+  showModal();
+};
+
